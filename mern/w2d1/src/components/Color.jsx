676995827956e246749b71bfbd83768879668020
Brendan Stanton <bmsenate@gmail.com>
2020-11-02 14:38:38 -0800

    added updated color functionality

--- mern/w2d1/src/components/Color.jsx
+++ mern/w2d1/src/components/Color.jsx
@@ -2,14 +2,18 @@ import React from 'react';
 
 
 
-const Color = ({color, idx, deleteColor}) => {
+const Color = ({color, idx, deleteColor, updateCompleted}) => {
 
     return (
         <div>
-            <section style={{color: color}}>
-                This color is: {color} |
+            <section style={{color: color.hue}}>
+                {color.task}, {color.hue}, {color.completed.toString()}
                 <button onClick={e => deleteColor(idx)}>Delete!</button>
-                <input type="checkbox"/>
+                <input
+                    type="checkbox"
+                    checked={color.completed}
+                    onChange={e => updateCompleted(idx)}
+                />
             </section>
         </div>
     )
--- mern/w2d1/src/components/NewColor.jsx
+++ mern/w2d1/src/components/NewColor.jsx
@@ -5,19 +5,38 @@ import React, {useState} from 'react';
 const NewColor = (props) => {
 
     const [color, setColor] = useState("");
+    const [task, setTask] = useState("");
 
     const createColor = (event) => {
         event.preventDefault();
-        props.addColor(color);
+        props.addColor({hue: color, task: task, completed: false});
         setColor("");
+        setTask("");
     }
+
+    // const colorChanger = (e) => {
+    //     setColor(e.target.value);
+    // }
 
     return (
         <div>
             <p>Hello this is the NewColor component!</p>
             <h1>YOUR COLOR IS: {color}</h1>
             <form onSubmit={createColor}>
-                <input type="text" value={color} onChange={e => setColor(e.target.value)} />
+                <p>
+                    Color: <input
+                            type="text"
+                            value={color}
+                            onChange={e => setColor(e.target.value)}
+                        />
+                </p>
+                <p>
+                    Task: <input
+                            type="text"
+                            value={task}
+                            onChange={e => setTask(e.target.value)}
+                        />
+                </p>
                 <input type="submit" value="Add a color!" />
             </form>
         </div>
